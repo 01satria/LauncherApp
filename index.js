97b318a1ebb8944f92397f239b7a4354ba9c82b0
Satria <see.cloudys@gmail.com>
2026-02-15 14:57:28 +0700

    new

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// index.js (root)
 import {AppRegistry} from 'react-native';
-import App from './src/App';  // ✅ Import langsung dari App.tsx
+import App from './src/App';  // ✅ Atau './src' juga bisa
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
