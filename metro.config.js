c7d35ed08bba546e2be07dbf9b01d30c3194e8be
Satria <see.cloudys@gmail.com>
2026-02-15 14:47:09 +0700

    new

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,10 @@
+// metro.config.js
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  resolver: {
+    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'], // ✅ Tambahkan ts & tsx
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
