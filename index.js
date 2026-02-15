c101653ea400a43d65f013332d9caff129870769
Satria <see.cloudys@gmail.com>
2026-02-15 15:09:09 +0700

    new

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// index.js
+/**
+ * @format
+ */
+
 import {AppRegistry} from 'react-native';
-import App from './App';  // ✅ File lama di root
+import App from './app/src/App.tsx';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
